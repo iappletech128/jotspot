ec32ff67ba8104ba7e0478c445e869eb034c6aa1
Ryan Bliss <alisabliss@Maddalyn.local>
2013-10-31 19:15:09 -0700

    Initial Commit

---
+++ JotSpot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JotSpot
+//
+//  Created by Ryan Bliss on 10/31/13.
+//  Copyright (c) 2013 iappletech128. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ JotSpot/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  JotSpot
+//
+//  Created by Ryan Bliss on 10/31/13.
+//  Copyright (c) 2013 iappletech128. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
